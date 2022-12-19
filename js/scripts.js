8f3532f81cadc29d6f8c32c6b3ac0ba9a2671b97
JosephHo7 <118572699+JosephHo7@users.noreply.github.com>
2022-12-18 19:15:34 -0500

    Add loop for DOM
    Create a loop to display pokemon names on the DOM

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,9 @@
 let pokemonList = [
     {name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison']},
     {name: 'Charmander', height: 0.6, types: ['fire', 'ground']},
-    {name: 'Squirtle', height: 0.5, types: ['water', 'dragon']},
+    {name: 'Squirtle', height: 0.5, types: ['water', 'dragon']}
 ];
+
+for (let i = 0; i < pokemonList.length; i++) {
+    document.write('<p>',pokemonList[i].name + ' ' + '(height:' + pokemonList[i].height + ')','</p>')
+}
