c35ab1cbb6822ee9c1bc54142945a4d6e2d2b3c8
JosephHo7 <118572699+JosephHo7@users.noreply.github.com>
2022-12-18 19:22:16 -0500

    emphasize pokemon height
    add a conditional loop to emphasize pokemon with height greater than 1

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,19 @@
 let pokemonList = [
     {name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison']},
-    {name: 'Charmander', height: 0.6, types: ['fire', 'ground']},
+    {name: 'Charmander', height: 1.6, types: ['fire', 'ground']},
     {name: 'Squirtle', height: 0.5, types: ['water', 'dragon']}
 ];
 
+//Create a loop to display pokemon names on the DOM
+// for (let i = 0; i < pokemonList.length; i++) {
+//     document.write('<p>',pokemonList[i].name + ' ' + '(height:' + pokemonList[i].height + ')','</p>')
+// }
+
+//Add loop with conditional to highlight any pokemon with height greater than 1
 for (let i = 0; i < pokemonList.length; i++) {
-    document.write('<p>',pokemonList[i].name + ' ' + '(height:' + pokemonList[i].height + ')','</p>')
+    if (pokemonList[i].height < 1)
+        document.write('<p>',pokemonList[i].name + ' ' + '(height:' + pokemonList[i].height + ')','</p>')
+    else {
+        document.write('<p>',pokemonList[i].name + ' ' + '(height:' + pokemonList[i].height + ')' + ' - Wow, that\'s big!','</p>')
+    }
 }
