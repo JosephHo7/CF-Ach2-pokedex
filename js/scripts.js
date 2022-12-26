3480693c54921a88b9d378a1e98f8b0e4ce4e4d2
JosephHo7 <118572699+JosephHo7@users.noreply.github.com>
2022-12-25 22:45:54 -0500

    change for to forEach function

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 let pokemonList = [
-    {name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison']},
-    {name: 'Charmander', height: 1.6, types: ['fire', 'ground']},
-    {name: 'Squirtle', height: 0.5, types: ['water', 'dragon']}
+    {name: 'Bulbasaur', height: 0.7, types: ['grass', ' poison']},
+    {name: 'Charmander', height: 1.6, types: ['fire', ' ground']},
+    {name: 'Squirtle', height: 0.5, types: ['water', ' dragon']}
 ];
 
 //Create a loop to display pokemon names on the DOM
@@ -9,11 +9,16 @@ let pokemonList = [
 //     document.write('<p>',pokemonList[i].name + ' ' + '(height:' + pokemonList[i].height + ')','</p>')
 // }
 
-//Add loop with conditional to highlight any pokemon with height greater than 1
-for (let i = 0; i < pokemonList.length; i++) {
-    if (pokemonList[i].height < 1)
-        document.write('<p>',pokemonList[i].name + ' ' + '(height:' + pokemonList[i].height + ')','</p>')
-    else {
-        document.write('<p>',pokemonList[i].name + ' ' + '(height:' + pokemonList[i].height + ')' + ' - Wow, that\'s big!','</p>')
-    }
-}
+// // Add loop with conditional to highlight any pokemon with height greater than 1
+// for (let i = 0; i < pokemonList.length; i++) {
+//     if (pokemonList[i].height < 1)
+//         document.write('<p>',pokemonList[i].name + ' ' + '(height:' + pokemonList[i].height + ')','</p>')
+//     else {
+//         document.write('<p>',pokemonList[i].name + ' ' + '(height:' + pokemonList[i].height + ')' + ' - Wow, that\'s big!','</p>')
+//     }
+// }
+
+//use forEach function instead of for 
+pokemonList.forEach(function(pokemon){
+    document.write('<p> ', pokemon.name + ' (height:' + pokemon.height + ') ' + ' types: ' + pokemon.types,' </p>');
+});
