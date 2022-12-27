b1d36d619b8eaa5f5895b2121bc368c4222b5d82
JosephHo7 <118572699+JosephHo7@users.noreply.github.com>
2022-12-26 23:18:34 -0500

    addListItem to code

--- js/scripts.js
+++ js/scripts.js
@@ -14,11 +14,29 @@ function getAll() {
     return pokemonList;
 }
 
+function addListItem(pokemon) {
+    let pokedexDisplayList = document.querySelector('.pokemon-list');
+    let listItem = document.createElement('li');
+    let button = document.createElement('button');
+    button.innerText = pokemon.name;
+    button.classList.add('pokedex-button');
+    listItem.appendChild(button);
+    pokedexDisplayList.appendChild(listItem);
+} 
+
 return {
     getAll: getAll,
-    add: add
+    add: add,
+    addListItem: addListItem
 }
 }) ()
+
+pokemonRepository.add({name: 'Pikachu', height: 0.4, types: ['electric', 'ground'] });
+
+pokemonRepository.getAll().forEach(function(pokemon) {
+    pokemonRepository.addListItem(pokemon);
+})
+
 
 //Create a loop to display pokemon names on the DOM
 // for (let i = 0; i < pokemonList.length; i++) {
@@ -34,11 +52,8 @@ return {
 //     }
 // }
 
-pokemonRepository.add({name: 'Pikachu', height: 0.4, types: ['electric', 'ground'] });
-
 //use forEach function instead of for 
-pokemonRepository.getAll().forEach(function(pokemon){
-    document.write('<p> ', pokemon.name + ' (height:' + pokemon.height + ') ' + ' types: ' + pokemon.types,' </p>');
-});
-
+// pokemonRepository.getAll().forEach(function(pokemon){
+//     document.write('<p> ', pokemon.name + ' (height:' + pokemon.height + ') ' + ' types: ' + pokemon.types,' </p>');
+// });
 
