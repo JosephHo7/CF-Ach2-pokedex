9d5597a73f4a9a60af1a9218e289a8c3903443de
JosephHo7 <118572699+JosephHo7@users.noreply.github.com>
2023-01-01 14:41:59 -0500

    delete old js code

--- js/scripts.js
+++ js/scripts.js
@@ -48,22 +48,4 @@ pokemonRepository.getAll().forEach(function(pokemon) {
 })
 
 
-//Create a loop to display pokemon names on the DOM
-// for (let i = 0; i < pokemonList.length; i++) {
-//     document.write('<p>',pokemonList[i].name + ' ' + '(height:' + pokemonList[i].height + ')','</p>')
-// }
-
-// // Add loop with conditional to highlight any pokemon with height greater than 1
-// for (let i = 0; i < pokemonList.length; i++) {
-//     if (pokemonList[i].height < 1)
-//         document.write('<p>',pokemonList[i].name + ' ' + '(height:' + pokemonList[i].height + ')','</p>')
-//     else {
-//         document.write('<p>',pokemonList[i].name + ' ' + '(height:' + pokemonList[i].height + ')' + ' - Wow, that\'s big!','</p>')
-//     }
-// }
-
-//use forEach function instead of for 
-// pokemonRepository.getAll().forEach(function(pokemon){
-//     document.write('<p> ', pokemon.name + ' (height:' + pokemon.height + ') ' + ' types: ' + pokemon.types,' </p>');
-// });
 
